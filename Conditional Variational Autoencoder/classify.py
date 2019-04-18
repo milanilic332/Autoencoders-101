69813cf85d6bae66decba870caf8f83143dddf38
Milan Ilic <milan.ilic@everseen.com>
2019-04-18 16:49:15 +0200

    Refactored code

--- Conditional Variational Autoencoder/classify.py
+++ Conditional Variational Autoencoder/classify.py
@@ -26,105 +26,96 @@ BATCH_SIZE = 16
 LATENT_DIM = 2
 
 
-# Reparameterization trick
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+# Build VAE and return layers needed by other functions
+def build_model():
+    def reparameterization_trick(args):
+        mean, log_var = args
+        batch = K.shape(mean)[0]
+        dim = K.int_shape(mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return mean + K.exp(0.5 * log_var) * epsilon
 
+    # Loss function (Reconstruction loss + KL between q(z | x) and N(0, 1)
+    def vae_loss(y_true, y_pred):
+        reconstruction_loss = IMG_HEIGHT * IMG_WIDTH * binary_crossentropy(K.flatten(input_image), K.flatten(outputs))
+        kl_loss = -0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        return K.mean(reconstruction_loss + kl_loss)
 
-# Loss function (Reconstruction loss + KL between q(z | x) and N(0, 1)
-def func_vae_loss(y_true, y_pred):
-    reconstruction_loss = IMG_HEIGHT*IMG_WIDTH*binary_crossentropy(K.flatten(input_image), K.flatten(outputs))
-    kl_loss = -0.5*K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-    return K.mean(reconstruction_loss + kl_loss)
+    # Build CVAE architecture
+    input_image = Input(shape=IMG_SHAPE)
+    input_cond = Input(shape=(10,))
 
+    encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(input_image)
+    encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(encoding)
+    encoding = MaxPooling2D((2, 2))(encoding)
 
-# Build CVAE architecture
-input_image = Input(shape=IMG_SHAPE)
-input_cond = Input(shape=(10,))
+    encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
+    encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
+    encoding = MaxPooling2D((2, 2))(encoding)
 
-encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(input_image)
-encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(encoding)
-encoding = MaxPooling2D((2, 2))(encoding)
+    encoding = Flatten()(encoding)
+    encoding = Dense(256, activation='relu')(encoding)
 
-encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
-encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
-encoding = MaxPooling2D((2, 2))(encoding)
+    encoding = concatenate([encoding, input_cond])
 
-encoding = Flatten()(encoding)
-encoding = Dense(256, activation='relu')(encoding)
+    z_mean = Dense(LATENT_DIM)(encoding)
+    z_log_var = Dense(LATENT_DIM)(encoding)
 
-encoding = concatenate([encoding, input_cond])
+    z = Lambda(reparameterization_trick, output_shape=(LATENT_DIM,))([z_mean, z_log_var])
 
-z_mean = Dense(LATENT_DIM, name='z_mean')(encoding)
-z_log_var = Dense(LATENT_DIM, name='z_log_var')(encoding)
+    # Encoder model (Input: [image, one-hot label]; Output: mu and sigma)
+    encoder = Model([input_image, input_cond], [z_mean, z_log_var, z])
+    encoder.summary()
 
-z = Lambda(sampling, output_shape=(LATENT_DIM,), name='z')([z_mean, z_log_var])
+    latent = Input(shape=(LATENT_DIM,))
 
-# Encoder model (Input: [image, one-hot label]; Output: mu and sigma)
-encoder = Model([input_image, input_cond], [z_mean, z_log_var, z])
-encoder.summary()
+    decoding = concatenate([latent, input_cond])
 
-latent = Input(shape=(LATENT_DIM,))
+    decoding = Dense(256, activation='relu')(decoding)
 
-decoding = concatenate([latent, input_cond])
+    decoding = Dense(49, activation='relu')(decoding)
+    decoding = Reshape((7, 7, 1))(decoding)
 
-decoding = Dense(256, activation='relu')(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
+    decoding = UpSampling2D((2, 2))(decoding)
 
-decoding = Dense(49, activation='relu')(decoding)
-decoding = Reshape((7, 7, 1))(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
+    decoding = UpSampling2D((2, 2))(decoding)
 
-decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
-decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
-decoding = UpSampling2D((2, 2))(decoding)
+    outputs = Conv2DTranspose(filters=1, kernel_size=3, activation='sigmoid', padding='same')(decoding)
 
-decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
-decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
-decoding = UpSampling2D((2, 2))(decoding)
+    # Decoder model (Input: [n-dimensional point (sample), one-hot label]; Output: image)
+    decoder = Model([latent, input_cond], outputs, name='decoder')
+    decoder.summary()
 
-outputs = Conv2DTranspose(filters=1, kernel_size=3, activation='sigmoid', padding='same')(decoding)
+    outputs = decoder([encoder([input_image, input_cond])[2], input_cond])
 
-# Decoder model (Input: [n-dimensional point (sample), one-hot label]; Output: image)
-decoder = Model([latent, input_cond], outputs)
-decoder.summary()
+    # VAE model (Input: image; Output: reconstructed image)
+    vae = Model([input_image, input_cond], outputs, name='vae')
 
+    # Compiling model
+    vae.compile(optimizer=Adam(LR), loss=vae_loss)
+    vae.summary()
 
-outputs = decoder([encoder([input_image, input_cond])[2], input_cond])
-
-# VAE model (Input: image; Output: reconstructed image)
-vae = Model([input_image, input_cond], outputs)
-
-# Compiling model
-vae.compile(optimizer=Adam(LR), loss=func_vae_loss)
-vae.summary()
-
-# Loading fashion mnist dataset and preparing it for training
-(x_train, y_train_), (x_test, y_test_) = fashion_mnist.load_data()
-y_train = np.zeros((y_train_.shape[0], 10))
-y_train[np.arange(y_train_.shape[0]), y_train_] = 1
-
-y_test = np.zeros((y_test_.shape[0], 10))
-y_test[np.arange(y_test_.shape[0]), y_test_] = 1
-
-x_train = np.expand_dims(x_train, axis=-1)/255.0
-x_test = np.expand_dims(x_test, axis=-1)/255.0
+    return encoder, decoder, vae
 
 
 # Plotting clothes around (0, 0) for label [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-def plot_clothes():
-    n = 30
+def plot_clothes(decoder):
+    n = 15
     figure = np.zeros((IMG_WIDTH * n, IMG_HEIGHT * n))
 
     grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
     grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
+    label = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+
     for i, yi in enumerate(grid_x):
         for j, xi in enumerate(grid_y):
             z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict([z_sample, np.array([np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]) for _ in range(n)])])
+            x_decoded = decoder.predict([z_sample, np.array([np.array(label) for _ in range(n)])])
             cloth = x_decoded[0].reshape(IMG_WIDTH, IMG_HEIGHT)
             figure[i * IMG_WIDTH: (i + 1) * IMG_WIDTH, j * IMG_WIDTH: (j + 1) * IMG_WIDTH] = cloth
 
@@ -133,18 +124,43 @@ def plot_clothes():
     plt.show()
 
 
-if __name__ == '__main__':
+# Loading fashion mnist dataset and preparing it for training
+def build_dataset():
+    # Loading fashion mnist dataset and preparing it for training
+    (x_train, y_train_), (x_test, y_test_) = fashion_mnist.load_data()
+    y_train = np.zeros((y_train_.shape[0], 10))
+    y_train[np.arange(y_train_.shape[0]), y_train_] = 1
+
+    y_test = np.zeros((y_test_.shape[0], 10))
+    y_test[np.arange(y_test_.shape[0]), y_test_] = 1
+
+    x_train = np.expand_dims(x_train, axis=-1)/255.0
+    x_test = np.expand_dims(x_test, axis=-1)/255.0
+
+    return x_train, y_train, x_test, y_test
+
+
+def main():
+    encoder, decoder, vae = build_model()
+
+    x_train, y_train, x_test, y_test = build_dataset()
+
     # Defining training callbacks
-    tbCallback = TensorBoard(log_dir='logs/', batch_size=1)
-    esCallback = EarlyStopping(monitor='val_loss', patience=5)
-    mcCallback = ModelCheckpoint('models/cvae.h5', save_best_only=True, save_weights_only=False)
-    rlrCallback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6)
+    tb_callback = TensorBoard(log_dir='logs/', batch_size=1)
+    es_callback = EarlyStopping(monitor='val_loss', patience=5)
+    mc_callback = ModelCheckpoint('models/cvae.h5', save_best_only=True, save_weights_only=False)
+    rlr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6)
 
     # Training
     vae.fit([x_train, y_train], x_train,
             validation_data=([x_test, y_test], x_test),
             batch_size=BATCH_SIZE,
             epochs=EPOCHS,
-            callbacks=[tbCallback, mcCallback, rlrCallback, esCallback])
+            callbacks=[tb_callback, mc_callback, rlr_callback, es_callback])
 
-    plot_clothes()
+    if LATENT_DIM == 2:
+        plot_clothes(decoder)
+
+
+if __name__ == '__main__':
+    main()
--- Variational Autoencoder/classify.py
+++ Variational Autoencoder/classify.py
@@ -11,6 +11,7 @@ from keras import backend as K
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D
 from scipy.stats import norm
 
 
@@ -20,89 +21,110 @@ IMG_HEIGHT = 28
 IMG_CHANNELS = 1
 IMG_SHAPE = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
 
-EPOCHS = 15
+EPOCHS = 50
 LR = 1e-4
-BATCH_SIZE = 16
-LATENT_DIM = 2
+BATCH_SIZE = 128
+LATENT_DIM = 3
 
 
-# Reparameterization trick
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+def build_model():
+    def reparameterization_trick(args):
+        mean, log_var = args
+        batch = K.shape(mean)[0]
+        dim = K.int_shape(mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return mean + K.exp(0.5 * log_var) * epsilon
 
+    # Loss function (Reconstruction loss + KL between q(z | x) and N(0, 1)
+    def vae_loss(y_true, y_pred):
+        reconstruction_loss = IMG_HEIGHT * IMG_WIDTH * binary_crossentropy(K.flatten(inputs), K.flatten(outputs))
+        kl_loss = -0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        return K.mean(reconstruction_loss + kl_loss)
 
-# Loss function (Reconstruction loss + KL between q(z | x) and N(0, 1)
-def func_vae_loss(y_true, y_pred):
-    reconstruction_loss = IMG_HEIGHT*IMG_WIDTH*binary_crossentropy(K.flatten(inputs), K.flatten(outputs))
-    kl_loss = -0.5*K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-    return K.mean(reconstruction_loss + kl_loss)
+    # Build VAE architecture
+    inputs = Input(shape=IMG_SHAPE)
 
+    encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(inputs)
+    encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(encoding)
+    encoding = MaxPooling2D((2, 2))(encoding)
 
-# Build VAE architecture
-inputs = Input(shape=IMG_SHAPE, name='encoder_input')
+    encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
+    encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
+    encoding = MaxPooling2D((2, 2))(encoding)
 
-encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(inputs)
-encoding = Conv2D(128, (5, 5), activation='relu', padding='same')(encoding)
-encoding = MaxPooling2D((2, 2))(encoding)
+    encoding = Flatten()(encoding)
+    encoding = Dense(256, activation='relu')(encoding)
 
-encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
-encoding = Conv2D(128, (3, 3), activation='relu', padding='same')(encoding)
-encoding = MaxPooling2D((2, 2))(encoding)
+    z_mean = Dense(LATENT_DIM)(encoding)
+    z_log_var = Dense(LATENT_DIM)(encoding)
 
-encoding = Flatten()(encoding)
-encoding = Dense(256, activation='relu')(encoding)
+    z = Lambda(reparameterization_trick, output_shape=(LATENT_DIM,))([z_mean, z_log_var])
 
-z_mean = Dense(LATENT_DIM, name='z_mean')(encoding)
-z_log_var = Dense(LATENT_DIM, name='z_log_var')(encoding)
+    # Encoder model (Input: image; Output: mu and sigma)
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
 
-z = Lambda(sampling, output_shape=(LATENT_DIM,), name='z')([z_mean, z_log_var])
+    latent_inputs = Input(shape=(LATENT_DIM,))
 
-# Encoder model (Input: image; Output: mu and sigma)
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-encoder.summary()
+    decoding = Dense(256, activation='relu')(latent_inputs)
 
-latent_inputs = Input(shape=(LATENT_DIM,), name='z_sampling')
+    decoding = Dense(49, activation='relu')(decoding)
+    decoding = Reshape((7, 7, 1))(decoding)
 
-decoding = Dense(256, activation='relu')(latent_inputs)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
+    decoding = UpSampling2D((2, 2))(decoding)
 
-decoding = Dense(49, activation='relu')(decoding)
-decoding = Reshape((7, 7, 1))(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
+    decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
+    decoding = UpSampling2D((2, 2))(decoding)
 
-decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
-decoding = Conv2DTranspose(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(decoding)
-decoding = UpSampling2D((2, 2))(decoding)
+    outputs = Conv2DTranspose(filters=1, kernel_size=3, activation='sigmoid', padding='same')(decoding)
 
-decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
-decoding = Conv2DTranspose(filters=128, kernel_size=(5, 5), activation='relu', padding='same')(decoding)
-decoding = UpSampling2D((2, 2))(decoding)
+    # Decoder model (Input: n-dimensional point (sample); Output: image)
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
 
-outputs = Conv2DTranspose(filters=1, kernel_size=3, activation='sigmoid', padding='same', name='decoder_output')(decoding)
+    outputs = decoder(encoder(inputs)[2])
 
-# Decoder model (Input: n-dimensional point (sample); Output: image)
-decoder = Model(latent_inputs, outputs, name='decoder')
-decoder.summary()
+    # VAE model (Input: image; Output: reconstructed image)
+    vae = Model(inputs, outputs, name='vae')
 
-outputs = decoder(encoder(inputs)[2])
+    # Compiling model
+    vae.compile(optimizer=Adam(LR), loss=vae_loss)
+    vae.summary()
 
-# VAE model (Input: image; Output: reconstructed image)
-vae = Model(inputs, outputs, name='vae')
+    return encoder, decoder, vae
 
-# Compiling model
-vae.compile(optimizer=Adam(LR), loss=func_vae_loss)
-vae.summary()
 
 # Loading fashion mnist dataset and preparing it for training
-(x_train, _), (x_test, _) = fashion_mnist.load_data()
-x_train = np.expand_dims(x_train, axis=-1)/255.0
-x_test = np.expand_dims(x_test, axis=-1)/255.0
+def build_dataset():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = np.expand_dims(x_train, axis=-1)/255.0
+    x_test = np.expand_dims(x_test, axis=-1)/255.0
+
+    return x_train, y_train, x_test, y_test
+
+
+def plot_latent(encoder, data):
+    samples = encoder.predict(data[0])[0]
+
+    cmap = plt.get_cmap(name='plasma')
+    colors = [list(cmap(i)) for i in np.linspace(0, 1, 10)]
+
+    color_dict = dict(zip([i for i in range(10)], colors))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for xy, c in zip(samples, [[color_dict[y]] for y in data[1]]):
+        ax.scatter(xy[0], xy[1], xy[2], c=c)
+
+    plt.show()
 
 
 # Plotting clothes around (0, 0)
-def plot_clothes():
+def plot_clothes(decoder):
     n = 30
     figure = np.zeros((IMG_WIDTH * n, IMG_HEIGHT * n))
 
@@ -118,21 +140,34 @@ def plot_clothes():
 
     plt.figure(figsize=(10, 10))
     plt.imshow(figure)
+
     plt.show()
 
 
-if __name__ == '__main__':
+def main():
+    encoder, decoder, vae = build_model()
+
+    x_train, y_train, x_test, y_test = build_dataset()
+
     # Defining training callbacks
-    tbCallback = TensorBoard(log_dir='logs/', batch_size=1)
-    esCallback = EarlyStopping(monitor='val_loss', patience=5)
-    mcCallback = ModelCheckpoint('models/vae.h5', save_best_only=True, save_weights_only=False)
-    rlrCallback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6)
+    tb_callback = TensorBoard(log_dir='logs/', batch_size=1)
+    es_callback = EarlyStopping(monitor='val_loss', patience=5)
+    mc_callback = ModelCheckpoint('models/vae.h5', save_best_only=True, save_weights_only=False)
+    rlr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6)
 
     # Training
     vae.fit(x_train, x_train,
             validation_data=(x_test, x_test),
             batch_size=BATCH_SIZE,
             epochs=EPOCHS,
-            callbacks=[tbCallback, mcCallback, rlrCallback, esCallback])
+            callbacks=[tb_callback, mc_callback, rlr_callback, es_callback])
 
-    plot_clothes()
+    if LATENT_DIM == 3:
+        plot_latent(encoder, [x_test[:500], y_test[:500]])
+
+    if LATENT_DIM == 2:
+        plot_clothes(decoder)
+
+
+if __name__ == '__main__':
+    main()
